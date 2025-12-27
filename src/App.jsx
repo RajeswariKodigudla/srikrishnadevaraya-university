9a30729779ca6afb0f81c118200ba4ea619def06
RajeswariKodigudla <rajeswarikodigudla47@gmail.com>
2025-12-27 20:42:04 +0530

    update app routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,16 @@
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+<>
+  <TopNotice />
+  <Header />
+  {/* <Navbar /> */}
 
-import TopNotice from "./components/TopNotice";
-import Header from "./components/Header";
-import Navbar from "./components/Navbar";
-import Footer from "./components/Footer";
-import UniversityMoUs from "./pages/UniversityMoUs";
+  <Routes>
+    <Route path="/" element={<HomePage />} />
+    <Route path="/about/about-university" element={<AboutUniversity />} />
+    <Route path="/about/mission-vision" element={<MissionVision />} />
+    <Route path="/about/history" element={<History />} />
+    <Route path="/about/university-mous" element={<UniversityMoUs />} />
+    <Route path="*" element={<Navigate to="/" />} />
+  </Routes>
 
-import HomePage from "./pages/HomePage";
-import AboutUniversity from "./pages/AboutUniversity";
-import MissionVision from "./pages/MissionVision";
-import History from "./pages/History";
-
-function App() {
-  return (
-    <BrowserRouter basename="/srikrishnadevaraya-university">
-      <TopNotice />
-      <Header />
-      <Navbar />
-
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-
-        <Route path="/about/about-university" element={<AboutUniversity />} />
-        <Route path="/about/mission-vision" element={<MissionVision />} />
-        <Route path="/about/history" element={<History />} />
-        <Route path="*" element={<Navigate to="/" />} />
-<Route
-  path="/about/university-mous"
-  element={<UniversityMoUs />}
-/>
-      </Routes>
-
-      <Footer />
-    </BrowserRouter>
-  );
-}
-
-export default App;
+  <Footer />
+</>
